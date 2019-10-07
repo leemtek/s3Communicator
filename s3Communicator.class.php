dbf298afd86328cb165d66b5d880f6da7717cbc0
Gabe Tumbaga <djgtraxx@gmail.com>
2019-10-07 01:21:28 +0000

    got basic upload image working. just creates new right now, doesn't update existing yet...

---
+++ s3Communicator.class.php
@@ -0,0 +1,65 @@
+<?php
+//Pull in the secret sauce =)
+
+include_once(dirname(__FILE__) . "/_settings.php");
+
+// Installed From composer.json
+require 'vendor/autoload.php';
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+// /composer.json
+
+
+
+
+class S3Communicator {
+    private $s3Bucket;
+    private $s3CloudFrontURL;
+    private $s3;
+
+    //--------------------------------------------------------------------
+    // CONSTRUCT
+    //--------------------------------------------------------------------
+    function __construct($bucketName, $cloudFrontURL) {
+      $this->s3Bucket = $bucketName;
+      $this->s3CloudFrontURL = $cloudFrontURL;
+
+      $this->s3 = new S3Client([
+          'version' => 'latest',
+          'region'  => S3_REGION,
+          'credentials' => [
+            'key'    => S3_KEY,
+            'secret' => S3_SECRET,
+          ]
+      ]);
+    }
+
+    //--------------------------------------------------------------------
+    // DESTRUCT
+    //--------------------------------------------------------------------
+    function __destruct() {
+      //
+    }
+
+    //--------------------------------------------------------------------
+    // PUT
+    //--------------------------------------------------------------------
+    function upload_image($formTmpName, $newFileName = null) {
+      $milliseconds = round(microtime(true) * 1000);
+      $myFileName = (!$newFileName) ? $milliseconds . ".jpg" : $newFileName;
+      try {
+        $upload = $this->s3->upload($this->s3Bucket, $myFileName, fopen($formTmpName, 'rb'));
+      } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+      }
+      return $upload;
+    }
+
+    //--------------------------------------------------------------------
+    // GET BUCKET NAME
+    //--------------------------------------------------------------------
+    function get_bucket_name() {
+      return $this->s3Bucket;
+    }
+
+}
